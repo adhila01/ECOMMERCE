a771a08a9eb74bbef3084abcbe0221f0c8c15078
adhila01 <adhila9539@gmail.com>
2024-12-16 10:47:01 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,30 +1,32 @@
-from django.urls import path
-from .views import *
+"""
+URL configuration for djangoproject project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
-    path('index/', index),
-    path('register/', reg),
-    path('userlogin/', userlogin),
-    path('userprofile/', userprofile),
-    path('usersingleview/', usersingleview),
-    path('updateuserdetails/', updateuserdetails),
-    path('productupload/',productupload),
-    path('addtocart/<int:itemid>',addtocart),
-    path('cartdisplay/',Cartdisplay),
-    path('incdec/<int:itemid>',inc_dec),
-    path('deletecart/<int:cartid>',deletecart),
-    path('wishlist/<int:cartid>',wishli),
-    path('wishlistdisplay/',wishlistdisplay),
-    path('wislistdelete/',wishlistdelete),
-    path('addaddress/',addaddress),
-    path('delivery_details/',delivery_details),
-    path('summary/',summary),
-    path('create_order/',create_order),
-    path('order_view/',order_view),
-    path('order_cancel/<int:id>',order_cancel),
-    path('logout/',logout),
-    path('changepassword/',changepassword)
+    path('admin/', admin.site.urls),
+    path('ecommerce/',include('ecommerce.urls')),
 
 
 
-]
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+
+
+
